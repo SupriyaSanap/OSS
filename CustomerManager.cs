9ed7a3cc1ee89a00a0ba1f1dd4a47536680d1448
SupriyaSanap <108924701+SupriyaSanap@users.noreply.github.com>
2022-07-09 15:13:23 +0530

    Add files via upload

---
+++ CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMWebApp.Models
+{
+    public class CustomerManager
+    {
+        public static List<Customer> Loaddata()
+        {
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer { ID = 1, Name = "Knowit", ContactPerson = "Bakul joshi", ContactNumber = "1234567890" });
+            customers.Add(new Customer { ID = 2, Name = "Sunbeam", ContactPerson = "Swara joshi", ContactNumber = "1234567890" });
+            customers.Add(new Customer { ID = 3, Name = "Acts", ContactPerson = "Vaishnavi patil", ContactNumber = "1234567890" });
+            customers.Add(new Customer { ID = 4, Name = "Kp", ContactPerson = "Supriya Sanap", ContactNumber = "1234567890" });
+            // deserialization 
+            return customers;
+        }
+        public static bool SaveData(List <Customer> customers)
+        {
+            bool status = false;
+            // serialazation 
+            return status;
+
+
+        }
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMWebApp.Models
+{
+    [Serializable]
+    public class Employee
+    {
+       public int ID { get; set; }
+      public  string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string ContactNumber { get; set; }
+        
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public string Location { get; set; }
+
+
+    }
+}
