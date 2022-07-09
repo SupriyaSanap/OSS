c15f98351b4e1a970bb11ded89bcb7923e377a3d
SupriyaSanap <supriyasanap1397@gmail.com>
2022-07-09 15:34:23 +0530

    CRM Module Functionality

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.MVC;
+namespace CRM
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OSS
 this Repository contains all source codes for online shopping software
+
+welcome to Github
